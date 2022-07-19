3750791fbc145487a90fbdea69dd001ca1821cb2
akhmetovdev <akhmetovdev@gmail.com>
2022-07-19 21:26:07 +0600

    [common] upgrades

--- index.js
+++ index.js
@@ -4,72 +4,76 @@ const fs = require('fs');
 const path = require('path');
 const Web3 = require('web3');
 
-const fastify = require('fastify')({
-  logger: true,
-  http2: true,
-  https: {
-    allowHTTP1: true,
-    key: fs.readFileSync(path.join(__dirname, 'fastify.key')),
-    cert: fs.readFileSync(path.join(__dirname, 'fastify.cert'))
-  }
-});
-
-fastify.register(require('@fastify/cors'));
-fastify.register(require('@fastify/compress'));
-
 let latestBlock = undefined;
-let port = parseInt(process.env.PORT) || 8545;
-let web3WsUrl = process.env.WEB3_WS_URL;
-let clientConfig = { maxReceivedFrameSize: 1e9, maxReceivedMessageSize: 1e9 };
-let provider = new Web3.providers.WebsocketProvider(web3WsUrl, { clientConfig });
-let web3 = new Web3(provider);
 
-fastify.post('/', (request, reply) => {
-  const { id } = request.body;
+(async function () {
+  const fastify = require('fastify')({
+    logger: true,
+    http2: true,
+    https: {
+      allowHTTP1: true,
+      key: fs.readFileSync(path.join(__dirname, 'fastify.key')),
+      cert: fs.readFileSync(path.join(__dirname, 'fastify.cert'))
+    }
+  });
 
-  reply.header('access-control-allow-headers', '*');
-  reply.header('access-control-allow-methods', '*');
-  reply.header('access-control-allow-origin', '*');
-  reply.header('access-control-max-age', 86400);
-  reply.header('vary', 'Origin');
+  await fastify.register(require('@fastify/cors'));
+  await fastify.register(require('@fastify/compress'), { removeContentLengthHeader: false, encodings: ['gzip'] });
 
-  reply.send({ jsonrpc: '2.0', id, result: latestBlock });
-});
+  let port = parseInt(process.env.PORT) || 8545;
+  let web3WsUrl = process.env.WEB3_WS_URL;
+  let clientConfig = { maxReceivedFrameSize: 1e9, maxReceivedMessageSize: 1e9 };
+  let provider = new Web3.providers.WebsocketProvider(web3WsUrl, { clientConfig });
+  let web3 = new Web3(provider);
 
-web3.eth.getBlockNumber((err, blockNumber) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+  fastify.post('/', (request, reply) => {
+    const { id } = request.body;
 
-  web3.eth.getBlock(blockNumber, false, (err, block) => {
+    reply.header('access-control-allow-headers', '*');
+    reply.header('access-control-allow-methods', '*');
+    reply.header('access-control-allow-origin', '*');
+    reply.header('access-control-max-age', 86400);
+    reply.header('vary', 'Origin');
+    reply.type('application/json');
+
+    reply.compress({ jsonrpc: '2.0', id, result: latestBlock });
+  });
+
+  web3.eth.getBlockNumber((err, blockNumber) => {
     if (err) {
       fastify.log.error(err);
       process.exit(1);
     }
 
-    saveBlock(block);
-    fastify.log.info(`block = ${blockNumber}`);
-
-    web3.eth.subscribe('newBlockHeaders', (err, blockHeader) => {
-      if (err == null) {
-        web3.eth.getBlock(blockHeader.number, false, (err, block) => {
-          if (err == null) {
-            saveBlock(block);
-            fastify.log.info(`block = ${blockHeader.number}`);
-          }
-        });
-      }
-    });
-
-    fastify.listen({ port, host: '0.0.0.0' }, err => {
+    web3.eth.getBlock(blockNumber, false, (err, block) => {
       if (err) {
         fastify.log.error(err);
         process.exit(1);
       }
+
+      saveBlock(block);
+      fastify.log.info(`block = ${blockNumber}`);
+
+      web3.eth.subscribe('newBlockHeaders', (err, blockHeader) => {
+        if (err == null) {
+          web3.eth.getBlock(blockHeader.number, false, (err, block) => {
+            if (err == null) {
+              saveBlock(block);
+              fastify.log.info(`block = ${blockHeader.number}`);
+            }
+          });
+        }
+      });
+
+      fastify.listen({ port, host: '0.0.0.0' }, err => {
+        if (err) {
+          fastify.log.error(err);
+          process.exit(1);
+        }
+      });
     });
   });
-});
+})();
 
 function saveBlock(block) {
   latestBlock = {
